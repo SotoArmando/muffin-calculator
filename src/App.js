2c88aae8621ced74b1a1e46ad86230d044a8f927
SotoArmando <armando29@live.com>
2020-03-08 19:50:21 -0600

    feature calculations approach eslint fixes

--- src/App.js
+++ src/App.js
@@ -3,34 +3,32 @@ import './style.css';
 import Buttonpanel from './Components/buttonpanel';
 import Display from './Components/display';
 import calculate from './Logic/calculate';
+import calculator from './Logic/calculate';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { total: '', next: null, operation: null };
+    this.state = { result: '' };
   }
-
   handleClick = (name) => {
     debugger;
-    if (!this.state.calculator) {
-      let calculator = new calculate();
+    const { calculator } = this.state;
+    if (!calculator) {
       calculator.tap(name);
-      
-      this.setState({ displaynumber: calculator.getnumber(), calculator: calculator })
+      this.setState({ result: calculator.getnumber(), calculator: new calculate() })
     } else {
-      this.state.calculator.tap(name);
-      this.setState({ displaynumber: this.state.calculator.getnumber(), calculator: this.state.calculator })
+      calculator.tap(name);
+      this.setState({ result: calculator.getnumber(), calculator: calculator })
     }
-
   };
 
   render() {
     debugger;
-    const { displaynumber } = this.state;
+    const { result } = this.state;
     return (
       <div className="App">
         <link href="https://fonts.googleapis.com/css?family=Spartan:400,500,700&display=swap" rel="stylesheet" />
-        <Display key={'5' + new Date().getTime()} result={displaynumber} />
+        <Display key={'5' + new Date().getTime()} result={result} />
         <Buttonpanel key={'0' + new Date().getTime()} names={['AC', '+/-', '%', '÷']} clickHandler={this.handleClick} />
         <Buttonpanel key={'1' + new Date().getTime()} names={['7', '8', '9', 'X']} clickHandler={this.handleClick} />
         <Buttonpanel key={'2' + new Date().getTime()} names={['4', '5', '6', '-']} clickHandler={this.handleClick} />
