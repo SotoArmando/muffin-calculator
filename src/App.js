da7f58087cc101eb7655418948447a215fd3ac05
SotoArmando <armando29@live.com>
2020-03-08 19:43:31 -0600

    feature calculation added functional math calculitions

--- src/App.js
+++ src/App.js
@@ -15,28 +15,27 @@ class App extends React.Component {
     if (!this.state.calculator) {
       let calculator = new calculate();
       calculator.tap(name);
-      calculator.operate(name);
-      this.setState({ total: calculator.total, next: calculator.next, operation: calculator.operation, calculator: calculator })
+      
+      this.setState({ displaynumber: calculator.getnumber(), calculator: calculator })
     } else {
       this.state.calculator.tap(name);
-      this.state.calculator.operate(name);
-      this.setState({ total: this.state.calculator.total, next: this.state.calculator.next, operation: this.state.calculator.operation, calculator: this.state.calculator })
+      this.setState({ displaynumber: this.state.calculator.getnumber(), calculator: this.state.calculator })
     }
 
   };
 
   render() {
     debugger;
-    const { total } = this.state;
+    const { displaynumber } = this.state;
     return (
       <div className="App">
         <link href="https://fonts.googleapis.com/css?family=Spartan:400,500,700&display=swap" rel="stylesheet" />
-        <Display key={'5' + new Date().getTime()} result={total} />
+        <Display key={'5' + new Date().getTime()} result={displaynumber} />
         <Buttonpanel key={'0' + new Date().getTime()} names={['AC', '+/-', '%', '÷']} clickHandler={this.handleClick} />
         <Buttonpanel key={'1' + new Date().getTime()} names={['7', '8', '9', 'X']} clickHandler={this.handleClick} />
         <Buttonpanel key={'2' + new Date().getTime()} names={['4', '5', '6', '-']} clickHandler={this.handleClick} />
         <Buttonpanel key={'3' + new Date().getTime()} names={['1', '2', '3', '+']} clickHandler={this.handleClick} />
-        <Buttonpanel key={'4' + new Date().getTime()} names={['0', '.', '=']} />
+        <Buttonpanel key={'4' + new Date().getTime()} names={['0', '.', '=']} clickHandler={this.handleClick} />
       </div>
     )
   };
--- src/Logic/operate.js
+++ src/Logic/operate.js
@@ -3,20 +3,27 @@ const Big = require('big.js');
 
 export default function operate(numberOne, numberTwo, operation) {
   const operationssrt = ['+/-', '%', '÷', 'X', '-', '+'];
-
+  let x, y, z;
+  debugger
   switch (operation) {
     case operationssrt[0]:
-      return new Big(numberOne).multiply(-1);
+      return new Big(numberOne).mul(-1).toFixed();
     case operationssrt[1]:
-      return new Big(numberOne).mod(numberTwo);
+      return new Big(numberOne).mod(new Big(numberTwo)).toFixed();
     case operationssrt[2]:
-      return new Big(numberOne).div(numberTwo);
+      return new Big(numberOne).div(new Big(numberTwo)).toFixed();
     case operationssrt[3]:
-      return new Big(numberOne).multiply(numberTwo);
+      x = new Big(numberOne);
+      y = new Big(numberTwo);
+      z = x.mul(y).toFixed();  
+      return z;
     case operationssrt[4]:
-      return new Big(numberOne).minus(numberTwo);
+      x = new Big(numberOne);
+      y = new Big(numberTwo);
+      z = x.minus(y).toFixed(); 
+      return z;
     case operationssrt[5]:
-      return new Big(numberOne).plus(numberTwo);
+      return new Big(numberOne).plus(new Big(numberTwo)).toFixed();
     default:
       return 0;
   }
